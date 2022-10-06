af140dad55f18541061d06da7c9580297578a278
16-LucasAw <RealAkaliMainer@gmail.com>
2022-10-06 11:27:56 +0800

    Added CollisionsScripts
    -Added CollisionScripts
    -Added CoinCollectedText

---
+++ Assets/Scripts/CollisionsScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionsScript : MonoBehaviour
+{
+
+    public GameObject CoinCollected;
+    private int coinCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coins")
+        {
+            coinCount++;
+
+            CoinCollected.GetComponent<Text>().text = "Coin collected: " + coinCount;
+
+            Destroy(other.gameObject);
+        }
+    }
+}
